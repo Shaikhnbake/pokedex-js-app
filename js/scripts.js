cd119b34ee336ce37c59c819c487841d889a5854
Shaikhnbake <103222534+Shaikhnbake@users.noreply.github.com>
2022-04-26 17:32:29 -0400

    cleaned up old code
    removed all old JS notes and the Alert message

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,3 @@
-alert('Hello Pokemon Trainers!');
-// this creates a pop up message when loading screen
-
-/* this is all practice CODE
-
-    let myName = 'Najm Shaikh';
-    //let and var can be used interchangably. both create variables
-    //myName is the assigned name and Najm Shaikh is the assigned value!
-
-    document.write(myName);
-    //tells JS to write on document ((Webpage))
-
-
-    myName = 'WebDevHutJr. Ceo';
-    document.write(myName);
-
-    let size = 100;
-    let doubleSize = size * 2;
-    document.write(doubleSize);
-
-    let minSize = (doubleSize*2)-(size/2);
-    document.write(minSize);
-
-    let favoriteFood = '10 piece wing, 3 wedges, pint of yams and extra hot sauce';
-    document.write(favoriteFood);
-
-*/
-
 let pokemonList = [
   {
     name: 'Charmander',
