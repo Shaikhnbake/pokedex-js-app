8dc5d29dd83461eaa96d3f663c9ebc48ab22392f
Shaikhnbake <103222534+Shaikhnbake@users.noreply.github.com>
2022-04-26 17:29:53 -0400

    Removed all old code
    Removed old comments and the Alert message.

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,38 @@
-alert('Hello Pokemon Trainers!');
-// this creates a pop up message when loading screen
-
-/* this is all practice CODE
-
-    let myName = 'Najm Shaikh';
-    //let and var can be used interchangably. both create variables
-    //myName is the assigned name and Najm Shaikh is the assigned value!
-
-    document.write(myName);
-    //tells JS to write on document ((Webpage))
-
-
-    myName = 'WebDevHutJr. Ceo';
-    document.write(myName);
-
-    let size = 100;
-    let doubleSize = size * 2;
-    document.write(doubleSize);
-
-    let minSize = (doubleSize*2)-(size/2);
-    document.write(minSize);
-
-    let favoriteFood = '10 piece wing, 3 wedges, pint of yams and extra hot sauce';
-    document.write(favoriteFood);
-
-*/
-
 let pokemonList = [
   {
     name: 'Charmander',
     height: 0.6,
     weight: 8.5,
     types: ['Lizard','Fire'],
-  }
+  },
 
   {
     name: 'Charizard',
     height: 1.7,
     weight: 90.5,
     types: ['Lizard','Fire','Flying'],
-  }
+  },
 
   {
     name: 'Raichu',
     height: 0.8,
     weight: 30,
     types: ['Mouse','Electric'],
-  }
+  },
 
   {
     name: 'Pikachu',
     height: 0.4,
     weight: 6,
     types: ['Mouse','Electric'],
-  }
+  },
 
   {
     name: 'Zubat',
     height: 0.8,
     weight: 7.5,
     types: ['Bat','Poison','Flying'],
-  }
+  },
 
   {
     name: 'Dugtrio',
