5b0e2b127c06a85bd540eeb255d86c64c72bc1b5
Shaikhnbake <shaikhnajm21@gmail.com>
2022-05-14 19:07:04 -0400

    Created IIFE
    Added pokemonRepository to serve as IIFE that holds pokemonList.
    
    Also added two functions: add and getAll

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,68 @@
-let pokemonList = [
-  {
-    name: 'Charmander',
-    height: 0.6,
-    weight: 8.5,
-    types: ['Lizard','Fire'],
-  },
+let pokemonRepository = (function(){
+  let pokemonList = [
+    {
+      name: 'Charmander',
+      height: 0.6,
+      weight: 8.5,
+      types: ['Lizard','Fire'],
+    },
 
-  {
-    name: 'Charizard',
-    height: 1.7,
-    weight: 90.5,
-    types: ['Lizard','Fire','Flying'],
-  },
+    {
+      name: 'Charizard',
+      height: 1.7,
+      weight: 90.5,
+      types: ['Lizard','Fire','Flying'],
+    },
 
-  {
-    name: 'Raichu',
-    height: 0.8,
-    weight: 30,
-    types: ['Mouse','Electric'],
-  },
+    {
+      name: 'Raichu',
+      height: 0.8,
+      weight: 30,
+      types: ['Mouse','Electric'],
+    },
 
-  {
-    name: 'Pikachu',
-    height: 0.4,
-    weight: 6,
-    types: ['Mouse','Electric'],
-  },
+    {
+      name: 'Pikachu',
+      height: 0.4,
+      weight: 6,
+      types: ['Mouse','Electric'],
+    },
 
-  {
-    name: 'Zubat',
-    height: 0.8,
-    weight: 7.5,
-    types: ['Bat','Poison','Flying'],
-  },
+    {
+      name: 'Zubat',
+      height: 0.8,
+      weight: 7.5,
+      types: ['Bat','Poison','Flying'],
+    },
 
-  {
-    name: 'Dugtrio',
-    height: 0.7,
-    weight: 33.3,
-    types: ['Mole','Ground'],
-  }
-]
-
-
-
+    {
+      name: 'Dugtrio',
+      height: 0.7,
+      weight: 33.3,
+      types: ['Mole','Ground'],
     }
+  ];
+
+
+
+  function add(pokemon){
+    if (typeof pokemon === "object" &&
+    "name" in pokemon &&
+    "height" in pokemon &&
+    "weight" in pokemon &&
+    "types" in pokemon){
+    pokemonList.push(pokemon);
+  } else {
+    console.log("input is wrong");
   }
+}
+  function getAll(){
+    return pokemonList;
+  }
+
+  return{
+    add: add,
+    getAll: getAll
+  };
+})();
 
