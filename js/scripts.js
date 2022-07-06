39a62f9b64d0aed8e95c1b27e49e13411295e364
Shaikhnbake <shaikhnajm21@gmail.com>
2022-07-05 21:41:08 -0400

    Added EsLinter and Prettier
    Used the auto form save option to clean up code and create code consistency

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,37 @@
 // pokemonRepository is the overcompassing IIFE for this POKEDEX
-let pokemonRepository = (function(){
-
-// pokemonList is the Array the holds all the pokemon objects
+let pokemonRepository = (function() {
+  // pokemonList is the Array the holds all the pokemon objects
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
-// Add allows user to input new pokemon as long as data type is correct.
-  function add(pokemon){
+  // Add allows user to input new pokemon as long as data type is correct.
+  function add(pokemon) {
     if (
       typeof pokemon === "object" &&
       "name" in pokemon &&
       "detailsUrl" in pokemon
-    ){
-    pokemonList.push(pokemon);
-  } else {
-    console.log("input is wrong");
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("input is wrong");
+    }
   }
-}
 
-// getAll returns all objects in pokemonList Array
-  function getAll(){
+  // getAll returns all objects in pokemonList Array
+  function getAll() {
     return pokemonList;
   }
 
-// Creates the structure of pokedex
-  function addListItem(pokemon){
+  // Creates the structure of pokedex
+  function addListItem(pokemon) {
     // assigns variable to HTML class
-    let pokedex = document.querySelector('.list-group');
+    let pokedex = document.querySelector(".list-group");
     // creates a list html object
-    let pokedexItem = document.createElement('li');
+    let pokedexItem = document.createElement("li");
     pokedexItem.classList.add("group-list-item");
 
-
     // creates a button per each List object
-    let button = document.createElement('button');
+    let button = document.createElement("button");
     // adds pokemon name to list and gives each button a specific class
     button.innerText = pokemon.name;
     button.classList.add("btn", "btn-lg", "btn-danger", "pokemon-list-button");
@@ -45,54 +43,59 @@ let pokemonRepository = (function(){
     // adds the list item to the whole pokedex
     pokedex.appendChild(pokedexItem);
     // will log pokemon details in console if User clicks on button!
-    button.addEventListener("click", function(event){
+    button.addEventListener("click", function(event) {
       showDetails(pokemon);
     });
   }
 
-
-  function showDetails(item){
-    loadDetails(item).then(function(){
+  function showDetails(item) {
+    loadDetails(item).then(function() {
       showModal(item);
     });
   }
 
-  function loadList(){
-    return fetch(apiUrl).then(function(response){
-      return response.json();
-    }).then(function(json){
-      json.results.forEach(function(item){
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        json.results.forEach(function(item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function(e) {
+        console.error(e);
       });
-    }).catch(function(e){
-      console.error(e);
-    })
   }
 
-  function loadDetails(item){
+  function loadDetails(item) {
     let url = item.detailsUrl;
-    return fetch(url).then(function(response){
-      return response.json();
-    }).then(function(details){
-      // add details to item list
-      item.imageUrl = details.sprites.front_default;
-      item.name = details.name;
-      item.height = details.height;
-      item.weight = details.weight;
+    return fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(details) {
+        // add details to item list
+        item.imageUrl = details.sprites.front_default;
+        item.name = details.name;
+        item.height = details.height;
+        item.weight = details.weight;
 
-      let types = [];
-      details.types.forEach((item) => types.push(item.type.name));
-      item.types = types;
-    }).catch(function(e){
-      console.error(e);
-    });
+        let types = [];
+        details.types.forEach(item => types.push(item.type.name));
+        item.types = types;
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
   }
 
-  function showModal(item){
+  function showModal(item) {
     let modalBody = $(".modal-body");
     let modalTitle = $(".modal-title");
     let modalHeader = $(".modal-header");
@@ -123,14 +126,12 @@ let pokemonRepository = (function(){
 })();
 
 //loads data from api
-pokemonRepository.loadList().then(function(){
-// forEach loop goes through pokemonList and creates Pokedex.
-  pokemonRepository.getAll().forEach(function (pokemon){
+pokemonRepository.loadList().then(function() {
+  // forEach loop goes through pokemonList and creates Pokedex.
+  pokemonRepository.getAll().forEach(function(pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
 });
-
-
 
 // OPTIONAL: WRITE FUNCTION THAT ADDS IMAGE URL from list items as actual images on HTML Page
 
